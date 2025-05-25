1045fac10bef2b261d0a5de6eb02501c124ca7b4
darthvader55 <71563965+darthvader55@users.noreply.github.com>
2025-05-25 10:56:32 +0000

    updates to training.py

--- ModelFiles/training_eval.py
+++ ModelFiles/training_eval.py
@@ -19,7 +19,7 @@ model.fit(X_train, y_train)
 
 # evaluation
 print("Evaluating Model Now..")
-y_pred = mind.predict(X_test)
+y_pred = model.predict(X_test)
 r2 = r2_score(y_test, y_pred)
 
 print(f"Model R² Score on Test Data: {r2:.4f}")
