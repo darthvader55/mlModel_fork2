0f2c9e66586cc0a975fe195a0f24f0de1fc949d9
darthvader55 <71563965+darthvader55@users.noreply.github.com>
2025-05-25 11:14:30 +0000

    adding flaskApp and Dockerfile

---
+++ ModelFiles/flaskApp.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from joblib import load
+import pandas as pd
+import numpy as np
+import pickle
+
+app=Flask(__name__)
+model=load("HousingModel.pkl")
+
+@app.route('/')
+def welcome():
+    return "Welcome Everyone To The House Price Predictor Webpage"
+
+@app.route('/predict')
+def predict_price():
+    try:
+        bedrooms = float(request.args.get('bedrooms'))
+        baths = float(request.args.get('baths'))
+        area = float(request.args.get('area'))
+
+        # arranging same way model was trained on
+        input_features = np.array([[bedrooms, baths, area]])  
+
+        prediction = model.predict(input_features)
+        prediction = np.round(prediction, 2)
+
+        return f"The predicted house price is ${prediction[0]:,.2f}"
+
+    except TypeError:
+        return "Missing one or more required query parameters: area, bedrooms, baths"
+
+    except ValueError:
+        return "Invalid input values. Please ensure bedrooms, baths and area are numeric."
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
