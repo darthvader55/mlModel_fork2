2214229658eb3f6b0300754e3fd412fdc8888506
darthvader55 <71563965+darthvader55@users.noreply.github.com>
2025-05-25 10:21:24 +0000

    flask update

--- flaskApp.py
+++ flaskApp.py
@@ -13,10 +13,22 @@ def welcome():
 
 @app.route('/predict')
 def predict_price():
-    area=request.args.get('area_sqft')
-    prediction=model.predict([[float(area)]])
-    prediction =np.round(prediction,2)
-    return "The predicted house price is" + str(prediction)
+    try:
+        bedrooms = float(request.args.get('bedrooms'))
+        baths = float(request.args.get('baths'))
+        area = float(request.args.get('area'))
+
+        # arranging same way model was trained on
+        input_features = np.array([[bedrooms, baths, area]])  
+
+        prediction = model.predict(input_features)
+        prediction = np.round(prediction, 2)
+
+    except TypeError:
+        return "Missing one or more required query parameters: area, bedrooms, baths"
+
+    except ValueError:
+        return "Invalid input values. Please ensure bedrooms, baths and area are numeric."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
